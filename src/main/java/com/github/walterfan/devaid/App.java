11543395cc1fd6c74681ba547bac35f2cae14ac9
Walter <walterfan@qq.com>
2013-07-28 19:45:42 +0800

    add maven skeleton files

---
+++ src/main/java/com/github/walterfan/devaid/App.java
@@ -0,0 +1,13 @@
+package com.github.walterfan.devaid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
