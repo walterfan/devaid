e68fdfcfb354fb90e8e611908514986ffee0c915
walterfan <walterfan@qq.com>
2014-07-02 18:46:22 +0800

    revised cassandra test program

--- src/test/java/com/github/walterfan/devaid/CassandraTest.java
+++ src/test/java/com/github/walterfan/devaid/CassandraTest.java
@@ -3,11 +3,14 @@ package com.github.walterfan.devaid;
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Host;
 import com.datastax.driver.core.Metadata;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Session;
 
 
 public class CassandraTest {
    private Cluster cluster;
-
+   private Session session = null;
+   
    public void connect(String node) {
       cluster = Cluster.builder()
             .addContactPoint(node)
@@ -19,6 +22,14 @@ public class CassandraTest {
          System.out.printf("Datatacenter: %s; Host: %s; Rack: %s\n",
                host.getDatacenter(), host.getAddress(), host.getRack());
       }
+      
+      session = cluster.connect();
+   }
+   
+   public ResultSet executeSql(String sql) {
+	   if(null == session) return null;
+	   return  session.execute(sql);
+	   
    }
 
    public void close() {
@@ -28,6 +39,9 @@ public class CassandraTest {
    public static void main(String[] args) {
 	   CassandraTest client = new CassandraTest();
        client.connect("10.224.57.163");
+       client.executeSql("CREATE KEYSPACE waltertest WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };");
+       client.executeSql("CREATE TABLE waltertest.users ( userid bigint PRIMARY KEY, email text, username text);");
+       
        client.close();
    }
 }
