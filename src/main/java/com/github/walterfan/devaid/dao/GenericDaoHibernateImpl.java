6e04ba530d7f64e8569d496b261b68c1be5c6316
walterfan <walterfan@qq.com>
2015-03-24 08:40:05 +0800

    add hibernate and ibatis

--- src/main/java/com/github/walterfan/devaid/dao/GenericDaoHibernateImpl.java
+++ src/main/java/com/github/walterfan/devaid/dao/GenericDaoHibernateImpl.java
@@ -15,5 +15,25 @@ public class GenericDaoHibernateImpl <T, PK extends Serializable>
     implements GenericDao<T, PK> {
     private Class<T> type;
 
+    @Override
+    public PK create(T newInstance) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public T read(PK id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(T transientObject) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(T persistentObject) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     
 }
