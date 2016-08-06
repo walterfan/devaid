19946e26b2b3bdf843f3b9d9fe75e8fea269110e
Walter Fan (yafan) <yafan@cisco.com>
2016-08-06 08:59:33 +0800

    remove redundant files

--- src/main/java/com/github/walterfan/devaid/biz/LinkService.java
+++
@@ -1,5 +0,0 @@
-package com.github.walterfan.devaid.biz;
-
-public class LinkService {
-
-}
--- src/test/java/com/github/walterfan/ssh2/SshClient.java
+++
@@ -1,9 +0,0 @@
-package com.github.walterfan.ssh2;
-
-/**
- * Created by walter on 5/22/16.
- */
-public class SshClient {
-
-
-}
