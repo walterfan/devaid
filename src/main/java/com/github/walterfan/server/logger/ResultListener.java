2c31766c14c9a87eff79a469eea415b4a1dd61d8
Walter Fan (yafan) <yafan@cisco.com>
2016-08-27 18:11:45 +0800

    remove test codes

--- src/main/java/com/github/walterfan/server/logger/ResultListener.java
+++
@@ -1,8 +0,0 @@
-package com.github.walterfan.server.logger;
-
-import com.github.walterfan.server.logger.message.ResultMessage;
-
-public interface ResultListener {
-	void onResult(ResultMessage msg);
-	void onException(Throwable throwable);
-}
